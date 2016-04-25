eb871d35b6b19c484acde7ad3434d2837ec10cf3
Jack Lawson <ajacksified@gmail.com>
2016-04-25 14:43:31 -0700

    Rebuild with atob

--- blueprints.config.js
+++ blueprints.config.js
@@ -3,18 +3,26 @@ module.exports = [{
   webpack: {
     entry: './lib/private.es6.js',
     output: {
-      generator: 'umd',
+      generator: 'simple',
       dest: './bin',
+    },
+    externals: {
+      atob: 'atob',
+      superagent: 'superagent',
+      url: 'url',
     },
     resolve: {
       generator: 'npm-and-modules',
-      extensions: ['', '.js', '.jsx', 'es6.js', '.json'],
+      extensions: ['', '.js', '.jsx', '.es6.js', '.json'],
     },
     loaders: [
       'esnextreact',
       'json',
     ],
     plugins: [
+      'production-loaders',
+      'set-node-env',
+      'minify-and-treeshake',
       'abort-if-errors',
     ],
   },
